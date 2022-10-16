87f09986f2b76b40a8ecc1d5758ed1d7c955b6bb
cratuss <ldoh92@gmail.com>
2022-10-17 03:04:47 +0900

    [Add] 예약폼 레이아웃

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,9 +4,16 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Main = () => {
+  const [userName, setUserName] = useState('');
+
   const navigate = useNavigate();
+
+  const changeHandler = word => {
+    setUserName(word.currentTarget.value);
+  };
 
   return (
     <MainBlock>
@@ -15,8 +22,15 @@ const Main = () => {
       </video>
       <InputGroup className='mb-3'>
         <InputGroup.Text id='inputGroup-sizing-default'>사용자 이름</InputGroup.Text>
-        <Form.Control aria-label='Default' aria-describedby='inputGroup-sizing-default' placeholder='노쇼한 사람 - 최철한, 김성식' />
-        <Button variant='primary' size='sm' onClick={() => navigate('./bookableList')}>
+        <Form.Control aria-label='Default' aria-describedby='inputGroup-sizing-default' placeholder='노쇼한 사람 - 최철한, 김성식' onChange={word => changeHandler(word)} />
+        <Button
+          variant='primary'
+          size='sm'
+          onClick={() => {
+            localStorage.setItem('token', JSON.stringify(userName));
+            navigate('./bookableList');
+          }}
+        >
           확인
         </Button>
       </InputGroup>
